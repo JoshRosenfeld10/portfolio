4055ac12a148011f63deb0e176079446e6b20716
Josh Rosenfeld <joshr@rogers.com>
2024-02-18 12:56:06 -0500

    changed project description

--- app/components/ProjectSection/projectData.ts
+++ app/components/ProjectSection/projectData.ts
@@ -9,7 +9,7 @@ export interface ProjectType {
 const projectData: ProjectType[] = [
     {
         title: 'Blog Posts!',
-        description: 'Full stack application that allows the user to create their own blog posts or view others.',
+        description: 'Full stack application that allows users to create their own blog posts or view others.',
         imagePath: '/images/1.png',
         githubLink: 'https://github.com/JoshRosenfeld10/Blog-Posts',
         previewLink: 'https://posts-project-jr.netlify.app/'
