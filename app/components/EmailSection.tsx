fdaa95c4f99cb9b14162d2da446f27fbc752a769
Josh Rosenfeld <joshr@rogers.com>
2024-02-18 19:59:04 -0500

    changed subject placeholder

--- app/components/EmailSection.tsx
+++ app/components/EmailSection.tsx
@@ -84,7 +84,7 @@ function EmailSection() {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Your subject..."
+                placeholder="Subject"
               />
             </div>
             <div className="mb-6">
